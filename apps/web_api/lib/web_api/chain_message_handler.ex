e659ad5e3d6b84cbc9cdb45e12067e39bc32117e
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2018-12-14 01:53:30 +0600

    WS start chain api

--- apps/web_api/lib/web_api/chain_message_handler.ex
+++ apps/web_api/lib/web_api/chain_message_handler.ex
@@ -9,14 +9,35 @@ defmodule WebApi.ChainMessageHandler do
   alias Chain.EVM.Notification
 
   @doc false
-  def start_link(_), do: GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  def start_link(_), do: GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
 
   @doc false
-  def init(_), do: {:ok, []}
+  def init(_), do: {:ok, %{}}
 
-  def handle_info(%Notification{id: id, event: :started, data: data}, state) do
-    IO.inspect(id)
-    IO.inspect(data)
+  # Add new event listener with socket_ref
+  def handle_cast({:add, event, pid, socket_ref}, state) do
+    list = Map.get(state, event, [])
+
+    case Enum.member?(list, pid) do
+      true ->
+        {:noreply, state}
+
+      false ->
+        new_state = Map.put(state, event, list ++ [{pid, socket_ref}])
+        {:noreply, new_state}
+    end
+  end
+
+  # Handle Notification from chain
+  def handle_info(%Notification{id: id, event: event, data: data}, state) do
+    # Check if we have direct requests for handler
+    if (list = Map.get(state, event, [])) && list != [] do
+      list
+      |> Enum.each(fn {pid, socket_ref} -> send(pid, {event, data, socket_ref}) end)
+    end
+
+    # Broadcasting event to direct channel
+    Phoenix.Endpoint.broadcast("chain:#{id}", event, data)
     {:noreply, state}
   end
 
@@ -24,4 +45,14 @@ defmodule WebApi.ChainMessageHandler do
     IO.inspect(msg)
     {:noreply, state}
   end
+
+  @doc """
+  Add new socket pid with socket ref to list of event listeners.
+  On event happening `WebApi.ChainMessageHandler` will send message to given `pid`
+  with such content: `{event, data, socket_ref}`
+  So you have to handle this message !
+  """
+  @spec notify_on(Chain.EVM.Notification.event(), pid, Phoenix.Channel.socket_ref()) :: :ok
+  def notify_on(event, pid, socket_ref),
+    do: GenServer.cast(__MODULE__, {:add, event, pid, socket_ref})
 end
--- apps/web_api/lib/web_api_web/channels/api_channel.ex
+++ apps/web_api/lib/web_api_web/channels/api_channel.ex
@@ -3,12 +3,14 @@ defmodule WebApiWeb.ApiChannel do
 
   alias Chain.EVM.Config
   alias WebApi.ChainHelper
+  alias WebApi.ChainMessageHandler
 
   def join(_, _, socket), do: {:ok, %{message: "Welcome to ExTestchain !"}, socket}
 
+  @doc """
+  Start new chain handler
+  """
   def handle_in("start", payload, socket) do
-    {:ok, pid} = ChainHelper.spawn_notification_handler(self(), socket_ref(socket))
-
     config = %Config{
       type: (Map.get(payload, "type") == "geth" && :geth) || :ganache,
       id: Map.get(payload, "id"),
@@ -16,11 +18,15 @@ defmodule WebApiWeb.ApiChannel do
       ws_port: Map.get(payload, "ws_port", 8546),
       db_path: Map.get(payload, "db_path", ""),
       accounts: Map.get(payload, "accounts", 1),
-      notify_pid: pid
+      block_mine_time: Map.get(payload, "block_mine_time", 0),
+      clean_on_stop: Map.get(payload, "clean_on_stop", false),
+      notify_pid: ChainMessageHandler
     }
 
+    ChainMessageHandler.notify_on(:started, self(), socket_ref(socket))
+
     {:ok, id} = Chain.start(config)
-    {:noreply, socket}
+    {:reply, {:ok, %{id: id}}, socket}
   end
 
   def handle_info({:started, data, ref}, socket) do
