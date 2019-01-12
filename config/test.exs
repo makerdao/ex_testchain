64c0c3802871ff1a25c152718573aca47329f525
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2019-01-12 22:40:30 +0200

    fix style

--- config/test.exs
+++ config/test.exs
@@ -1,6 +1,8 @@
 use Mix.Config
 
-config :chain, ganache_executable: System.get_env("GANACHE_EXECUTABLE") || Path.expand("#{__DIR__}/../../../priv/presets/ganache-cli/cli.js")
+config :chain,
+  ganache_executable:
+    System.get_env("GANACHE_EXECUTABLE") || Path.expand("#{__DIR__}/../../../priv/presets/ganache-cli/cli.js")
 
 config :logger,
   backends: [:console],
