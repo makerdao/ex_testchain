3e468718aafa71fcf3dcdadf7cb7c27edb3557d9
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2019-01-18 12:34:43 +0200

    fix configs for docker

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -8,3 +8,6 @@ config :chain, snapshot_base_path: "/opt/snapshots"
 config :chain, geth_password_file: "/opt/built/priv/presets/geth/account_password"
 config :chain, ganache_executable: "ganache-cli"
 config :chain, ganache_wrapper_file: "/opt/built/priv/presets/ganache/wrapper.sh"
+
+# Place where all dets DBs will be
+config :storage, dets_db_path: "/opt/chains"
