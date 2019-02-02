630b50a928d38b0bf42fcd331310f90d43062913
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2019-02-02 20:24:37 +0200

    updated config

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-# Basically this config will be used on realease building. 
+# Basically this config will be used on realease building.
 # And application will use this config for Docker build
 
 config :chain, base_path: "/opt/chains"
@@ -10,6 +10,8 @@ config :chain, ganache_executable: "ganache-cli"
 config :chain, ganache_wrapper_file: "/opt/built/priv/presets/ganache/wrapper.sh"
 
 config :chain, backend_proxy_node: :"testchain_backendgateway@testchain-backendgateway.local"
+
+config :chain, front_url: "testchain_backendgateway"
 
 # Place where all dets DBs will be
 config :storage, dets_db_path: "/opt/chains"
